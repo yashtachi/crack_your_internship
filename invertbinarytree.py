0d0684489f7fc4fc39f86e345a329588d2767146
yash <yashwanthmiryala1234@gmail.com>
2024-07-22 21:23:08 +0530

    day6

---
+++ invertbinarytree.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return
+        
+        root.left,root.right=root.right,root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
